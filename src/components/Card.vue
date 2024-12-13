5d079137a7b30a884cfc56d982bbf4e8ae7e6916
Axis <erlia@naver.com>
2024-12-14 00:47:08 +0900

    3

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
   
-    <div class="col">
+   
         <div class="card shadow-sm">
           <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
           <div class="card-body">
@@ -14,7 +14,7 @@
             </div>
           </div>
         </div>
-      </div>
+     
 
 </template>
 
@@ -27,5 +27,9 @@ export default{
 
 
 </script>
+
+<style scoped>
+
+</style>
 
 
